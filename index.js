b6c199e7a9558e9d194c0afd9c2d69cbfde26dff
Brandon Hyland <bhy@rams.colostate.edu>
2021-11-15 21:49:46 -0700

    hashing integration

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+const bcrypt = require('bcryptjs')
 const db = require('./dbConnectExec.js');
 
 const app = express(); 
+app.use(express.json());
 
 
 //arg 1 = port number, arg 2 is function we want to run
@@ -16,7 +17,43 @@ app.get("/", (req, res)=>{res.send("Api is Running")});
 // app.post();
 // app.put();
 
-app.get("/movies",(req, res)=>{
+app.post("/contacts", async (req, res)=>{
+    // res.send("/contacts called");
+// console.log("request.body", req.body);
+
+    let shopFK = req.body.shopFK;
+    let nameFirst = req.body.nameFirst;
+    let nameLast = req.body.nameLast;
+    let email = req.body.email;
+    let password = req.body.password;
+
+    let emailCheckQuery = `SELECT email FROM Guide WHERE email = '${email}'`
+
+    let existingUser = await db.executeQuery(emailCheckQuery);
+
+    // console.log("existing user", existingUser);
+
+    if (existingUser[0]){return res.status(409).send("duplicate email")};
+
+    let hashedPassword = bcrypt.hashSync(password);
+
+    // we need to delete all users with nonhashed password from database!!!!!!!!!!!
+
+    let insertQuery = `insert into Guide(ShopFK, FirstName, LastName, Email, Password)
+    values('${shopFK}', '${nameFirst}', '${nameLast}', '${email}', '${hashedPassword}')`;
+
+    db.executeQuery(insertQuery)
+    .then(()=>{
+        res.status(201).send()})
+    .catch((err)=>{
+        console.log('error in post /contacts', err);
+        res.status(500).send();
+    })
+
+})
+
+//if having trouble change /guides to /movies
+app.get("/guides",(req, res)=>{
     //get movies from database
     db.executeQuery("SELECT * FROM Guide LEFT JOIN Shop ON Shop.ShopPK = Guide.ShopFK")
     .then((theResults)=>{
@@ -27,3 +64,29 @@ app.get("/movies",(req, res)=>{
         res.status(500).send();
     })
 } )
+
+app.get("/movies/:pk", (req, res) => {
+
+    let pk = req.params.pk
+    console.log(pk);
+
+    let myQuery = `SELECT * FROM Guide
+    LEFT JOIN Shop
+    ON Shop.ShopPK = Guide.ShopFK
+    where GuidePK = ${pk}`
+
+    db.executeQuery(myQuery)
+    .then((result)=>{
+        console.log("result", result);
+        if (result[0]){
+            res.send(result[0])
+        }
+        else{
+            res.status(404).send("bad request");
+        }
+    })
+    .catch((err)=>{
+        console.log("error in /moives/:pk", err);
+        res.status(500).send();
+    });
+});
