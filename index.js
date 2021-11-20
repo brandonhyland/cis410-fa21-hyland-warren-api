b0bbdf88dfa4df37e66da5f97841172aeb20fa1c
Brandon Hyland <bhy@rams.colostate.edu>
2021-11-19 22:46:07 -0700

    update

--- index.js
+++ index.js
@@ -207,3 +207,96 @@ app.get("/movies/:pk", (req, res) => {
         res.status(500).send();
     });
 });
+
+app.get("/booking/:pk", (req, res) => {
+
+    let pk = req.params.pk
+    console.log(pk);
+
+    let myQuery = `SELECT * FROM Guide
+    LEFT JOIN Booking
+    ON Booking.GuideFK = Guide.GuidePK
+    where GuidePK = ${pk}`
+
+    db.executeQuery(myQuery)
+    .then((result)=>{
+        console.log("result", result);
+        if (result[0]){
+            res.send(result[0])
+        }
+        else{
+            res.status(404).send("bad request");
+        }
+    })
+    .catch((err)=>{
+        console.log("error in /booking/:pk", err);
+        res.status(500).send();
+    });
+});
+
+app.post("/contacts/logout", async (req, res)=>{
+    // console.log("/contacts/login called ", req.body)
+
+    //1. Data validation
+
+    let email = req.body.email;
+    let password = req.body.password;
+
+    if (!email || !password){
+        return res.status(400).send("Bad request")
+    }
+
+    //2. Check that user exists in dataabase
+
+    let query = `SELECT * FROM Guide WHERE email = '${email}'`;
+
+    let result;
+    try {
+    result = await db.executeQuery(query);
+    } catch(myError){
+      console.log("error in /contacts/login", myError)
+      return res.status(500).send();
+    }
+    // console.log("result", result);
+    if(!result[0]){return res.status(401).send("Invalid user credentials");
+    }
+
+    //3. Check password
+
+    let user = result[0];
+
+    if (!bcrypt.compareSync(password, user.Password)) {
+        return res.status(401).send("Invalid user credentials");
+    }
+
+    //4. Generate Token
+
+    let token = jwt.sign({pk: user.GuidePK}, rockwellConfig.JWT, {expiresIn: "60 minutes"});
+    console.log("token", token)
+
+    token = "logged out";
+
+    //5 save token in DB and send response
+
+    let setTokenQuery = `UPDATE Guide
+    SET token = 'Logged Out'
+    WHERE GuidePK = ${user.GuidePK}`;
+
+    try{
+        await db.executeQuery(setTokenQuery);
+
+        res.status(200).send({
+            token: token,
+            user:{
+                FirstName: user.FirstName,
+                LastName: user.LastName,
+                Email: user.Email,
+                GuidePK: user.GuidePK
+            }
+        })
+    }  
+    catch(myError){
+        console.log("Error in setting user token", myError)
+        res.send(500).send();
+    }    
+})         
